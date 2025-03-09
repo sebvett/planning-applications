697acf72bc4d85929fe5b1e2a89b8c13cc0b01e7
Lauren Thomas <lcthomas1212@gmail.com>
2025-03-09 11:58:14 +0000

    Finished spider so that it succesfully runs on a given single URL

--- planning_applications/spiders/lpas/wandsworth_single.py
+++ planning_applications/spiders/lpas/wandsworth_single.py
@@ -11,11 +11,30 @@ class WandsworthSingleSpider(scrapy.Spider):
     allowed_domains = ['planning.wandsworth.gov.uk']
     start_urls = ['https://planning.wandsworth.gov.uk/Northgate/PlanningExplorer/Generic/StdDetails.aspx?PT=Planning%20Applications%20On-Line&TYPE=PL/PlanningPK.xml&PARAM0=1140089&XSLT=/Northgate/PlanningExplorer/SiteFiles/Skins/Wandsworth/xslt/PL/PLDetails.xslt&FT=Planning%20Application%20Details&PUBLIC=Y&XMLSIDE=&DAURI=PLANNING']
 
+
+     # Define our special rows, and which handler needs to be used
+    special_rows = {
+        "Application Registered": "date", 
+        "Comments Until": "date",
+        "Date of Committee": "date",
+        "Decision": "status_plus_date",
+        "Appeal Lodged": "date",
+        "Location Co ordinates": "easting_northing"
+    }
+    
+    # define our special handlers 
+    special_handlers = {
+        "date": lambda self, row: self._handle_date(row),
+        "status_plus_date": lambda self, row: self._handle_status_plus_date(row),
+        "easting_northing": lambda self, row: self._handle_easting_northing(row)
+    }
+    
     def __init__(self, url=None, *args, **kwargs):
         super(WandsworthSingleSpider, self).__init__(*args, **kwargs)
         # Ensure the URL is passed
         if url:
             self.start_urls = [url]
+    
     
     def parse(self, response):
         application_details = {}
@@ -25,41 +44,6 @@ class WandsworthSingleSpider(scrapy.Spider):
             heading = dataview.xpath('h1/text()').get().split('\xa0')[0]
             # Split the string on the non-breaking space character - everything before spaces is consistent
             application_details[heading] = self._parse_dataview(dataview)
-
-
-        # try: 
-        #     # Get the application registered date
-        #     application_registered = response.xpath(
-        #         '//ul[@class="list"]//li[div/span[text()="Application Registered"]]/div/text()[normalize-space()]'
-        #     ).get().strip()
-        #     # Parse the string to a datetime object
-        #     application_details['date_registered'] = datetime.strptime(application_registered, '%d/%m/%Y')
-        # except ValueError:
-        #     if "XPath error" in str(ValueError):
-        #         # If the XPath error is raised, we don't have a value - NaN it
-        #         application_details['date_registered'] = nan
-        #         self.logger.error(f"XPath error: {ValueError}")
-        #     elif "does not match format" in str(ValueError):
-        #         # If we can't parse the datetime, store the raw string
-        #         application_details['date_registered'] = application_registered
-        #         self.logger.warning(f"Could not parse date: {ValueError}")
-        #     else:
-        #         # If we have no idea what happened, log it!
-        #         self.logger.error(f"Unknown error: {ValueError}")
-
-        # # Get the application number
-        # application_details['application_number'] = response.xpath(
-        #     '//ul[@class="list"]//li[div/span[text()="Application Number"]]/div/text()[normalize-space()]'
-        # ).get().strip()
-        # application_details['address'] = response.xpath(
-        #     '//ul[@class="list"]//li[div/span[text()="Site Address"]]/div/text()[normalize-space()]'
-        # ).get().strip()
-        # application_details['proposal'] = response.xpath(
-        #     '//ul[@class="list"]//li[div/span[text()="Proposal"]]/div/text()[normalize-space()]'
-        # ).get().strip()
-        # application_details['status']  = response.xpath(
-        #     '//ul[@class="list"]//li[div/span[text()="Current Status"]]/div/text()[normalize-space()]'
-        # ).get().strip()
 
         print(application_details)
 
@@ -75,7 +59,7 @@ class WandsworthSingleSpider(scrapy.Spider):
             if row_name in self.special_rows:
                 handler = self.special_handlers.get(self.special_rows[row_name])
                 if handler:
-                    row_value = handler(row)
+                    row_value = handler(self, row)
                 else:
                     self.logger.error(f"No handler for {row_name}")
                     return {}
@@ -93,30 +77,29 @@ class WandsworthSingleSpider(scrapy.Spider):
             return handler(self, dataview)
         
 
-    def _handle_date(self, row:Selector) -> datetime:
+    def _handle_date(self, row:Selector) -> datetime|str:
         ### Handle DATE-ONLY rows - not status+date
         date_string = row.xpath('div/text()')[1].get().split('\xa0')[0]
-        return datetime.strptime(date_string, '%d/%m/%Y')
+        try:
+            return datetime.strptime(date_string, '%d/%m/%Y')
+        except: 
+            return f"error parsing date: got {date_string}" 
     
     def _handle_status_plus_date(self, row:Selector) -> dict:
         ### Handle STATUS+DATE rows
         [status, date_string] = row.xpath('div/text()')[1].get().split('\xa0')
-        return {'status': status, 'date': datetime.strptime(date_string.strip(), '%d/%m/%Y')}
+        try: 
+            date = datetime.strptime(date_string.strip(), '%d/%m/%Y')
+        except:
+            date = f"error parsing date: got {date_string}"
+        return {'status': status, 'date': date}
 
-    # Define our special rows, and which handler needs to be used
-    special_rows = {
-        "Application Registered": "date", 
-        "Comments Until": "date",
-        "Date of Committee": "date",
-        "Decision": "status_plus_date",
-        "Appeal Lodged": "date",
-    }
-
-    # Define our special handlers
-    special_handlers = {
-        "date": _handle_date,
-        "status_plus_date": _handle_status_plus_date,
-    }
+    def _handle_easting_northing(self, row:Selector) -> dict:
+        ### Handle EASTING/NORTHING (UK grid coordinates) rows
+        # Easting/Northing is on the 15th "ul/li" row
+        # Received as a list ['Easting', '530014', 'Northing', '177704', '']
+        [_, easting, _,  northing, _] = row.xpath('div/text()')[1].get().split('\xa0')
+        return {'easting': easting, 'northing': northing}
 
     # def _parse_heading(self, dataview) -> dict:
 
@@ -128,6 +111,9 @@ class WandsworthSingleSpider(scrapy.Spider):
         
 
  #to do: parse comments as well (separate page)
+ #TODO -- Grab the planning application documents (including comments) PDFs. Link is always https://planning2.wandsworth.gov.uk/planningcase/comments.aspx?case=$APPLICATION_NUMBER
+ #       -- The documents page runs on JS, so that's annoying. Can we reverse engineer the API?
+ #       -- For now, we'
 
 
 
